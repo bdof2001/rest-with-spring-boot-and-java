9aee2e92399239e30e3ee916f52629f08724ed25
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-16 15:18:51 +0100

    Refactoring Application Code by Separating Concepts

--- src/main/java/nandes/pt/controllers/MathController.java
+++ src/main/java/nandes/pt/controllers/MathController.java
@@ -1,14 +1,20 @@
-package nandes.pt;
+package nandes.pt.controllers;
 
 import nandes.pt.exceptions.UnsupportedMathOperationException;
+import nandes.pt.math.SimpleMath;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import static nandes.pt.converters.NumberConverter.covertToDouble;
+import static nandes.pt.converters.NumberConverter.isNumeric;
 
 @RestController
 public class MathController {
 
     private final AtomicLong counter = new AtomicLong();
+
+    private SimpleMath math = new SimpleMath();
 
     @GetMapping(value = "/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
@@ -16,7 +22,7 @@ public class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+        return math.sum(covertToDouble(numberOne), covertToDouble(numberTwo));
     }
 
     @GetMapping(value = "/subtraction/{numberOne}/{numberTwo}")
@@ -25,7 +31,7 @@ public class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return covertToDouble(numberOne) - covertToDouble(numberTwo);
+        return math.subtraction(covertToDouble(numberOne), covertToDouble(numberTwo));
     }
 
     @GetMapping(value = "/multiplication/{numberOne}/{numberTwo}")
@@ -34,7 +40,7 @@ public class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return covertToDouble(numberOne) * covertToDouble(numberTwo);
+        return math.multiplication(covertToDouble(numberOne), covertToDouble(numberTwo));
     }
 
     @GetMapping(value = "/division/{numberOne}/{numberTwo}")
@@ -43,7 +49,7 @@ public class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return covertToDouble(numberOne) / covertToDouble(numberTwo);
+        return math.division(covertToDouble(numberOne), covertToDouble(numberTwo));
     }
 
     @GetMapping(value = "/mean/{numberOne}/{numberTwo}")
@@ -52,7 +58,7 @@ public class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return (covertToDouble(numberOne) + covertToDouble(numberTwo)) / 2;
+        return math.mean(covertToDouble(numberOne), covertToDouble(numberTwo));
     }
 
     @GetMapping(value = "/squareRoot/{number}")
@@ -61,19 +67,6 @@ public class MathController {
         if (!isNumeric(number)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return Math.sqrt(covertToDouble(number));
-    }
-
-    public static Double covertToDouble(String strNumber) {
-        if (strNumber == null) return 0d;
-        String number = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)) return Double.parseDouble(number);
-        return 1.0d;
-    }
-
-    public static boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+        return math.squareRoot(covertToDouble(number));
     }
 }
