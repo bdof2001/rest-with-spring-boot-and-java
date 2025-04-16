75650d969408be6d36277edfa81a869e1c36b6f2
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-16 14:45:06 +0100

    Changing the Controller to Sum and Defining the Validation Methods and Running the Application

---
+++ src/main/java/nandes/pt/MathController.java
@@ -0,0 +1,33 @@
+package nandes.pt;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = "/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }
+
+    public static Double covertToDouble(String strNumber) {
+        if (strNumber == null) return 0d;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 1.0d;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
