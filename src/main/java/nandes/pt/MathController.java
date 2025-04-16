0c180bb3bd1abfbb2def5d8ab5c3c8b1c75b61db
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-16 14:58:21 +0100

    Handling Exceptions with @ControllerAdvice, @ResponseStatus, and @ExceptionHandler

--- src/main/java/nandes/pt/MathController.java
+++ src/main/java/nandes/pt/MathController.java
@@ -1,5 +1,6 @@
 package nandes.pt;
 
+import nandes.pt.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -13,7 +14,7 @@ public class MathController {
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value");
         }
         return covertToDouble(numberOne) + covertToDouble(numberTwo);
     }
