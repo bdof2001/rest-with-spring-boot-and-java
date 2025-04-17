1dacb269b8023d4101987721439c2a51ae0bf309
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-17 17:49:15 +0100

    Testing the Create Operation

--- src/test/java/nandes/pt/integrationtests/controllers/PersonControllerIntegrationTest.java
+++ src/test/java/nandes/pt/integrationtests/controllers/PersonControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package nandes.pt.integrationtests.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
@@ -9,11 +11,13 @@ import io.restassured.specification.RequestSpecification;
 import nandes.pt.config.TestConfigs;
 import nandes.pt.integrationtests.testcontainers.AbstractIntegrationTest;
 import nandes.pt.model.Person;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -24,11 +28,12 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
     private static Person person0;
 
     @BeforeAll
-    void setup() {
+    static void setup() {
         objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
         specification = new RequestSpecBuilder()
-                .setBasePath("")
+                .setBasePath("/person")
                 .setPort(TestConfigs.SERVER_PORT)
                 .addFilter(new RequestLoggingFilter(LogDetail.ALL))
                 .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
@@ -36,4 +41,39 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
         person0 = new Person("Leandro", "Costa", "leandrocosta@gmail.com", "Rua do Campo", "Male");
     }
 
+    @Test
+    @Order(1)
+    @DisplayName("JUnit integration given Person Object when Create one Person should Return a Person Object")
+    void integrationTestGivenPersonObject_when_CreateOnePerson_ShouldReturnAPersonObject() throws JsonMappingException, JsonProcessingException {
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(person0)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        Person createdPerson = objectMapper.readValue(content, Person.class);
+
+        person0 = createdPerson;
+
+        assertNotNull(createdPerson);
+
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+        assertNotNull(createdPerson.getEmail());
+
+        assertTrue(createdPerson.getId() > 0);
+        assertEquals("Leandro", createdPerson.getFirstName());
+        assertEquals("Costa", createdPerson.getLastName());
+        assertEquals("Rua do Campo", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+        assertEquals("leandrocosta@gmail.com", createdPerson.getEmail());
+    }
 }
