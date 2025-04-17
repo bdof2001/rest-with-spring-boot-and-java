54bfa7452643108eea0f7e6aef3e8df71d954bb5
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-17 15:36:25 +0100

    Preparing Test Infrastructure with Testcontainers

---
+++ src/test/java/nandes/pt/config/TestConfigs.java
@@ -0,0 +1,8 @@
+package nandes.pt.config;
+
+public class TestConfigs {
+
+    public static final int SERVER_PORT = 8888;
+    public static final String CONTENT_TYPE_JSON = "application/json";
+
+}
